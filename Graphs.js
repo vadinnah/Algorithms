7dcfe783d6184e7d606fba25b4742bb636373602
vadinnah <avandre6@gmail.com>
2019-05-05 10:57:21 -0400

    Had to modify the bfs_mit to be as complete as the bfs_textbook. The original implementation doesn't give information for nodes not reachabl from s.

--- Graphs.js
+++ Graphs.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // #region AdjacencyList, AdjacencyMatrix, Graph classes
 
 var AdjacencyList = function(nodes, edges, isDirected) {
@@ -52,7 +54,7 @@ var AdjacencyMatrix = function(nodes, edges, isDirected) {
 	}
 }
 
-var Graph = function(V,E,isDirected=false) {
+var GraphWithoutPrototyping = function(V,E,isDirected=false) {
 	var me = this;
 	this.vertices = function(nodes){
 		if (nodes instanceof Array) {
@@ -69,7 +71,39 @@ var Graph = function(V,E,isDirected=false) {
 		}
 	}(V);
 	this.edges = E;
-	this.isDirected = isDirected;
+    this.isDirected = isDirected;
+
+    this.adjacencyList = function() {
+        return new AdjacencyList(this.vertices,this.edges, this.isDirected);
+    }
+
+    this.adjacencyMatrix = function() {
+        return new AdjacencyMatrix(this.vertices,this.edges, this.isDirected);
+    };
+
+    // even using Graph.prototype.constructor = Graph 
+    // won't prevent each instance from having separate
+    // copies of member functions.
+};
+
+var Graph = function(V,E,isDirected=false) {
+	var me = this;
+	this.vertices = function(nodes){
+		if (nodes instanceof Array) {
+			return nodes;
+		}
+		else {
+			//Assume nodes is a number
+			let indices = new Array(nodes);	
+			for(let i=0;i<nodes;)
+			{
+				indices[i] = ++i;
+			}
+			return indices;
+		}
+	}(V);
+	this.edges = E;
+    this.isDirected = isDirected;
 }; 
 
 Graph.prototype.adjacencyList = function() {
@@ -80,35 +114,55 @@ Graph.prototype.adjacencyMatrix = function() {
     return new AdjacencyMatrix(this.vertices,this.edges, this.isDirected);
 };
 
+Graph.prototype.vertexColor =  {
+    WHITE:'white',
+    GRAY:'gray',
+    BLACK:'black'
+};
+Object.freeze(Graph.prototype.vertexColor);
+
 /**
  * @summary implementation from MIT OCW Course 
  * @param {*} s starting vertex
  * @param {*} g Graph
  */
 Graph.prototype.bfs_mit = function(s) {
-    // Track distance of each node from start
+    
     let al = this.adjacencyList();
-    let levels = {};
-    levels[s]=0;
+    let V = this.vertices;
+
+    // Track distance of each node from start
+    let level = {};
     // Track the parent of each node from start
-    let parents = {};
-    parents[s]=null;
+    let parent = {};
     // Track the nodes we have visited
-    let frontier = [s];
+    let frontier = [];
+
+    //Handle those vertices not recheable from s
+    for(let u of V) {
+        level[u]=Infinity;
+        parent[u]=undefined;
+    }
+
+    level[s]=0;
+    parent[s]=null;
+    frontier.push(s);
+
     // Track the current level
     let i = 1;
+
     while(frontier && frontier.length>0) {
         // Track those nodes that will become a part of the next frontier
         let next = [];
         // Explore the nodes in the curent frontier
         for(let u of frontier) {
             for(let v of al[u]) {
-                if(levels[v]===undefined || levels[v]===null)
+                if(level[v]===Infinity)
                 {
                     // set the current level of the newly reached node
-                    levels[v]=i;
+                    level[v]=i;
                     // set the parents of the newly reach node
-                    parents[v]=u;
+                    parent[v]=u;
                     // mark newly reach node as a new frontier
                     next.push(v);
                 }
@@ -120,8 +174,11 @@ Graph.prototype.bfs_mit = function(s) {
         // Mark the level (distance from start) of the new frontier
         i++;
     }
-    return {levels,parents};
+    
+    return {distance: level,parent};
 };
+
+
 
 /**
  * @summary
@@ -136,17 +193,41 @@ Graph.prototype.bfs_textbook = function(s) {
     let distance = {};
     let parent = {};
     let queue = [];
-
+    let al = me.adjacencyList();
     let V = me.vertices;
-    let al = me.adjacencyList;
+    let vertexColor=me.vertexColor;
 
     for(let u of V) {
         if(u!==s) {
-            color[u]='white';
-
+            color[u]=vertexColor.WHITE;
+            distance[u]=Infinity;
+            //parent[u]=null; 
+            parent[u]=undefined; 
         }
     }
+
+    color[s]=vertexColor.GRAY;
+    distance[s]=0;
+    parent[s]=null;
+    queue.push(s);
+    
+    while(queue.length>0)
+    {
+        let u = queue.shift();
+        for(let v of al[u]) {
+            if(color[v]===vertexColor.WHITE) {
+                color[v]=vertexColor.GRAY;
+                distance[v]=distance[u]+1;
+                parent[v]=u;
+                queue.push(v);
+            }
+        }
+        color[u]=vertexColor.BLACK;
+    }
+    return {distance,parent};
 }
+
+//var WeightedGraph = function() {};
 
 var g1 = new Graph(
 	['v','s','w','q','t','x','z','y','r','u'],
@@ -178,10 +259,14 @@ var g2 = new Graph(
 for(let g of [g1,g2]) {
     console.log('\n----------------------------');
     console.log(g);
-    console.log(g.adjacencyList());
-    console.log(g.adjacencyMatrix());
+    console.log(g.bfs_mit(g.vertices[0]));
+    console.log(g.bfs_textbook(g.vertices[0]));
     console.log('----------------------------\n');
 }
 
+console.log(Graph.prototype);
+
+//console.log(Graph.prototype.constructor);
+//console.log(g1.adjacencyList===g2.adjacencyList);
 // console.log(g1.adjacencyList());
 //console.log(g1);
