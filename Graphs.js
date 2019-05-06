87fdced57851ee27ac8530a9941acb13e2bfd694
vadinnah <avandre6@gmail.com>
2019-05-05 23:12:16 -0400

    Topological sort and edge classification are now done by individual methods.

--- Graphs.js
+++ Graphs.js
@@ -201,56 +201,100 @@ Graph.prototype.bfs = function(s) {
     return {distance,parent};
 }
 
+/**
+ * @description performs dfs,topological_sort and classifies edges
+ */
+// Graph.prototype.dfs_eeverything = function(SV) {
+//     let me = this;
+
+//     //white=reached,gray=visiting,black=visited
+//     let color = {};
+//     let discoveryTime = {};
+//     let parents = {}
+//     let finishTime = {};
+//     let timer = 0;    
+//     let al = me.adjacencyList();
+//     let vc = me.vertexColor;
+//     let topologicalSort = [];
+//     let edgeClasses = new me.edgeClassifications();
+
+//     // define the recursed operations
+//     function dfs_visit(u) {
+//         color[u] = vc.GRAY;
+//         timer++;
+//         discoveryTime[u]=timer;
+//         for(let v of al[u]) {
+//             if (color[v]===vc.WHITE) {
+//                 parents[v]=u;
+//                 edgeClasses.leaf.push([u,v]);
+//                 dfs_visit(v);                
+//             }
+//             else if(color[v]===vc.GRAY) {
+//                 edgeClasses.back.push([u,v]);
+                
+//             }
+//             else {
+//                 if(finishTime[u]<finishTime[v]) {
+//                     edgeClasses.forward.push([u,v]);
+//                 }
+//                 else {
+//                     edgeClasses.cross.push([u,v]);
+//                 }
+//             }
+//         }
+//         color[u]=vc.BLACK;
+//         timer++;
+//         finishTime[u]=timer;
+//         topologicalSort.unshift(u);
+//     };
+
+//     // before graph exploration begins
+//     // mark all vertices as not yet explored
+//     for(let n of me.vertices) {
+//         color[n]=vc.WHITE;
+//         discoveryTime[n]=undefined;
+//         finishTime[n]=undefined;
+//         parents[n]=null;
+//     }
+
+//     // explore graph
+//     for(let n of SV) {
+//         if(color[n]===vc.WHITE) {
+//             dfs_visit(n);
+//         }
+//     }
+
+//     // if parents[u]=null, that means in this iteration
+//     // of dfs that vertex doesn't have a parent
+//     return {discoveryTime,finishTime,parents,topologicalSort,edgeClasses};
+// };
+
+
 Graph.prototype.dfs_explore = function(SV) {
     let me = this;
 
     //white=reached,gray=visiting,black=visited
     let color = {};
-    let discoveryTime = {};
-    let parents = {}
-    let finishTime = {};
-    let timer = 0;    
+    let parents = {}  
     let al = me.adjacencyList();
     let vc = me.vertexColor;
-    let topologicalSort = [];
-    let edgeClasses = new me.edgeClassifications();
 
     // define the recursed operations
     function dfs_visit(u) {
         color[u] = vc.GRAY;
-        timer++;
-        discoveryTime[u]=timer;
         for(let v of al[u]) {
             if (color[v]===vc.WHITE) {
                 parents[v]=u;
-                edgeClasses.leaf.push([u,v]);
                 dfs_visit(v);                
-            }
-            else if(color[v]===vc.GRAY) {
-                edgeClasses.back.push([u,v]);
-                
-            }
-            else {
-                if(discoveryTime[u]<discoveryTime[v]) {
-                    edgeClasses.forward.push([u,v]);
-                }
-                else {
-                    edgeClasses.cross.push([u,v]);
-                }
             }
         }
         color[u]=vc.BLACK;
-        timer++;
-        finishTime[u]=timer;
-        topologicalSort.push(u);
     };
 
     // before graph exploration begins
     // mark all vertices as not yet explored
     for(let n of me.vertices) {
         color[n]=vc.WHITE;
-        discoveryTime[n]=undefined;
-        finishTime[n]=undefined;
         parents[n]=null;
     }
 
@@ -263,7 +307,7 @@ Graph.prototype.dfs_explore = function(SV) {
 
     // if parents[u]=null, that means in this iteration
     // of dfs that vertex doesn't have a parent
-    return {discoveryTime,finishTime,parents,topologicalSort,edgeClasses};
+    return parents;
 };
 
 /**
@@ -317,6 +361,110 @@ Graph.prototype.isAcyclic = function() {
     // if parents[u]=null, that means in this iteration
     // of dfs that vertex doesn't have a parent
     return !backEdgeFound;
+};
+
+Graph.prototype.topological_sort = function() {
+    let me = this;
+
+    //white=reached,gray=visiting,black=visited
+    let color = {};    
+    let al = me.adjacencyList();
+    let vc = me.vertexColor;
+    let result = [];
+
+    // define the recursed operations
+    function dfs_visit(u) {
+        color[u] = vc.GRAY;
+        for(let v of al[u]) {
+            if (color[v]===vc.WHITE) {
+                dfs_visit(v);
+            }                
+        }
+        color[u]=vc.BLACK;
+        result.unshift(u);
+    };
+
+    // before graph exploration begins
+    // mark all vertices as not yet explored
+    for(let n of me.vertices) {
+        color[n]=vc.WHITE;
+    }
+
+    // explore graph
+    for(let n of me.vertices) {
+        if(color[n]===vc.WHITE) {
+            dfs_visit(n);
+        }
+    }
+
+    // if parents[u]=null, that means in this iteration
+    // of dfs that vertex doesn't have a parent
+    return result;
+};
+
+Graph.prototype.classify_edges = function() {
+    let me = this;
+
+    //white=reached,gray=visiting,black=visited
+    let color = {};
+    let discoveryTime = {};
+    //let parents = {}
+    let finishTime = {};
+    let timer = 0;    
+    let al = me.adjacencyList();
+    let vc = me.vertexColor;
+    //let topologicalSort = [];
+    let edgeClasses = new me.edgeClassifications();
+
+    // define the recursed operations
+    function dfs_visit(u) {
+        color[u] = vc.GRAY;
+        timer++;
+        discoveryTime[u]=timer;
+        for(let v of al[u]) {
+            if (color[v]===vc.WHITE) {
+                //parents[v]=u;
+                edgeClasses.leaf.push([u,v]);
+                dfs_visit(v);                
+            }
+            else if(color[v]===vc.GRAY) {
+                edgeClasses.back.push([u,v]);
+                
+            }
+            else {
+                if(finishTime[u]<finishTime[v]) {
+                    edgeClasses.forward.push([u,v]);
+                }
+                else {
+                    edgeClasses.cross.push([u,v]);
+                }
+            }
+        }
+        color[u]=vc.BLACK;
+        timer++;
+        finishTime[u]=timer;
+        //topologicalSort.push(u);
+    };
+
+    // before graph exploration begins
+    // mark all vertices as not yet explored
+    for(let n of me.vertices) {
+        color[n]=vc.WHITE;
+        discoveryTime[n]=undefined;
+        finishTime[n]=undefined;
+        // parents[n]=null;
+    }
+
+    // explore graph
+    for(let n of me.vertices) {
+        if(color[n]===vc.WHITE) {
+            dfs_visit(n);
+        }
+    }
+
+    // if parents[u]=null, that means in this iteration
+    // of dfs that vertex doesn't have a parent
+    return edgeClasses;
 }
 
 //var WeightedGraph = function() {};
