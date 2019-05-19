e3b6fbbc5a9329ba0e20c9051101f93d8ed7250c
vadinnah <avandre6@gmail.com>
2019-05-19 00:12:31 -0400

    Queue after validation.

--- dataStructures/Queue.js
+++ dataStructures/Queue.js
@@ -3,7 +3,26 @@ var Queue = function()
 {
     let me = this;
     me.list = new Array();
-    me.enqueue = function(e) {me.list.push(e)};
-    me.enqueueRange = function(range) {me.list = [...list,...range]};
-    me.dequeque = function(e) {return me.list.shift(e)};
 }
+
+Queue.prototype.enqueue = function(e) {
+    this.list.push(e)
+};
+Queue.prototype.enqueueRange = function(range) {
+    this.list = [...this.list,...range]
+};
+
+Queue.prototype.dequeue = function() {
+    let res = undefined;
+    if(this.list.length>0) {
+        res = this.list.shift();
+    }
+    return res;
+};
+
+var q = new Queue();
+
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueueRange([4,5,6]);
