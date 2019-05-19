71b865e83efeca06a27409cdbffc5ef03ab12e0b
vadinnah <avandre6@gmail.com>
2019-05-18 23:49:29 -0400

    Adding a data structures folder after grappling with Hackerrank problem "Libraries And Roads" made me realize that there are plent of data sets I need to know how to implement.

---
+++ dataStructures/DisjointSet.js
@@ -0,0 +1,55 @@
+
+var DisjointSet = function(V) {
+    this.Parent = {};
+    this.Rank = {};
+    this.Count = 0;
+
+    if(V instanceof Array) {
+        this.Count = V.length;
+        for(let v of V) {
+            this.Parent[v] = v;
+            this.Rank[v] = 0;
+        }
+    } else {
+        this.Count = V;
+        for(let v=1;v<=V;v++) {
+            this.Parent[v] = v;
+            this.Rank[v] = 0;
+        }
+    }  
+}
+
+DisjointSet.prototype.Find = function(v) {
+    let r = this.Parent[v];
+    let t = v;
+    while(r!==t) {
+        t = r;
+        r = this.Parent[t];
+    }
+    this.Parent[v]=r;
+    return r;
+}
+
+DisjointSet.prototype.Union = function(u,v) {
+    let uSet = this.Find(u);
+    let vSet = this.Find(v);
+    let rankOfUSet = this.Rank[u];
+    let rankOfVSet = this.Rank[v];
+
+    if(uSet===vSet) {
+        return;
+    }
+
+    if(rankOfUSet<rankOfVSet) {
+        this.Parent[uSet]=vSet;
+    }
+    else if(rankOfVSet<rankOfUSet) {
+        this.Parent[vSet]=uSet;
+    }
+    else {
+        this.Parent[uSet]=vSet;
+        this.Rank[vSet]++;
+    }
+}
+
+module.exports = DisjointSet;
