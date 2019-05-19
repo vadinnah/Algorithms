632150e315fa6740682ddfb20f9845070861bb7f
vadinnah <avandre6@gmail.com>
2019-05-18 23:51:52 -0400

    Fixed enqueue method.

--- dataStructures/Queue.js
+++ dataStructures/Queue.js
@@ -3,7 +3,7 @@ var Queue = function()
 {
     let me = this;
     me.list = new Array();
-    me.enqueue = function(e) {me.list.pop(e)};
+    me.enqueue = function(e) {me.list.push(e)};
     me.enqueueRange = function(range) {me.list = [...list,...range]};
     me.dequeque = function(e) {return me.list.shift(e)};
 }
